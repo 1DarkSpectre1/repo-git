145d76375b721402884f98a0c216635fe06ccfcb
1DarkSpectre1 <75720434+1DarkSpectre1@users.noreply.github.com>
2020-12-15 15:11:25 +0400

    Add files via upload

--- 2.2/main.js
+++ 2.2/main.js
@@ -1,4 +1,5 @@
-var storagedata = [
+"use strict"
+let storagedata = [
 	{ id: 1, title: "Стол", vol: 12 ,cost:100 },
 	{ id: 2, title: "Стул", vol: 300 ,cost:200 },
 	{ id: 3, title: "Книга", vol: 5 ,cost:1300},
@@ -7,12 +8,14 @@ var storagedata = [
 	{ id: 5, title: "Звездолёт", vol: 2,cost:1 }
 ];
 
-var basketdata = [];
+let basketdata = [
 
-var basketsummdata={id:'summ',title:"Стоимость товара",vol:0}
+	];
+
+let basketsummdata={id:'summ',title:"Стоимость товара",vol:0}
 
 function summ(data) {//нахождение общей стоимости товара в data 
-	summbs=0;
+	let summbs=0;
 	data.forEach(item=>{
 		summbs+=(item.vol*item.cost);
 	});
@@ -34,32 +37,30 @@ function refresh() {// обновление данных
 			$$("basket").refresh();
 			$$("storage").refresh();
 			$$("basketsumm").refresh();
+			
     	}
 
 function stbs(id,data1,data2) {// уменьшение количества товара в data1 с номером id и его увелечение в data2
-	ind=true
+	let ind=true;
+	let elemi;
 	//добавление к количеству товара и проверка есть ли такой товар в корзине
-	for (var i = 0; i < data2.length; i++) {
-		if (data2[i].id==id) {
-					data2[i].vol++;
+	for (let i = 0; i < data2.length; i++) {
+		if (data2[i].id == id) {
+					data2[i].vol++
 					ind=false;
 		}
 	}
 
-	for (var i = 0; i < data1.length; i++) {
+	for (let i = 0; i < data1.length; i++) {
 		if (data1[i].id== id) {
-			elemi=i;						//запоминаем номер элемента
+			if (ind) {
+				data2[data2.length]=Object.assign({},data1[i])//копируем элемент
+				data2[data2.length-1].vol=1;
+			}
 		}
 	}
 
-	//если товара нет в корзине создаём его
-	if (ind) {
-		data2[data2.length]=Object.assign({},data1[elemi])//копируем элемент
-		data2[data2.length-1].vol=1;
-	}
-
-
-	for (var i = 0; i < data1.length; i++) {
+	for (let i = 0; i < data1.length; i++) {
 
 		if (data1[i].id== id) {
 			data1[i].vol--;			//уменьшаем количство товара
