5ebd557523f81d187dd8982e1364ea268ee6c3ce
1DarkSpectre1 <75720434+1DarkSpectre1@users.noreply.github.com>
2020-12-23 11:56:10 +0400

    Add files via upload

---
+++ 3.2/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+    "flag"
+  "bufio"
+  "fmt"
+  "log"
+  "os"
+  "net/http"
+  "io/ioutil"
+  "net/url"
+    "strings"
+)
+
+
+
+func MakeRequest(line string, domain string, result string) {//get запрос и сохраняем html файл по пути result
+  resp, err := http.Get(line)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  body, err := ioutil.ReadAll(resp.Body)//считываем содержимое страницы
+  if err != nil {
+    log.Fatalln(err)
+  }
+  
+  if result!="" {//проверка на путь к результату 
+    err = os.MkdirAll(result, 0777)// создание директории
+    if err != nil {
+      log.Fatalln(err)
+    }
+  }
+
+  err=ioutil.WriteFile(result+domain + ".html", body, 0777)//запись в файл по пути result
+  if err != nil {
+    // Если произошла ошибка выводим ее в консоль
+    fmt.Println(err)
+  }
+
+}
+
+//считывание содержимое файла построчно
+func readLines (path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+func main() {
+
+
+
+  var filepath string
+    flag.StringVar(&filepath, "filepath", "url.txt", "a string var")// флаг для пути к файлу
+  
+  var result string
+    flag.StringVar(&result, "result", "", "a string var")// флаг для пути к результату
+  var logpath string
+  	flag.StringVar(&logpath, "logpath", "text.log", "a string var")// флаг для пути к логам
+    flag.Parse()
+
+    f, err := os.OpenFile(logpath,os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+      log.Println(err)
+    }
+    defer f.Close()
+    
+    var logger = log.New(f, "3.1: ", log.LstdFlags)
+    if logpath!="text.log" {
+      log.SetOutput(f) 
+    }
+    
+
+
+    logger.Println("********************Начало записи****************************** ")
+
+  lines, err := readLines(filepath)
+  if err != nil {
+    log.Fatalf("readLines: %s", err)
+  }
+
+  for i, line := range lines {
+    u, err := url.Parse(line)
+    if err != nil {
+      log.Fatal(err)
+    } 
+
+    //из адресса вытаскиваем домен для имени файла
+    parts := strings.Split(u.Hostname(), ".")
+    domain := parts[len(parts)-2 ]
+
+    logger.Println("Начало работы с:"+line)
+    if result!="" {
+      if !(strings.HasSuffix(result, "/")) {
+        MakeRequest(line,domain,result+string("/"))//делаем запрос и сохраняем файл
+      }
+    }else{
+    MakeRequest(line,domain,result)//делаем запрос и сохраняем файл
+    }
+    logger.Println("Окончена работа с:"+line)
+
+ 
+    var l=log.New(os.Stdout,"",log.Ldate|log.Ltime)
+    l.Println(i, line)
+    
+  }
+  logger.Println("********************Конец записи****************************** ")
+}
