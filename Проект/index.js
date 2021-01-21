a32c287b7c785a86fe8f2c25452db4806447d5ac
1DarkSpectre1 <75720434+1DarkSpectre1@users.noreply.github.com>
2021-01-21 17:58:16 +0400

    Add files via upload

--- Проект/index.js
+++ Проект/index.js
@@ -1,34 +1,7 @@
-
-
-webix.ui({
-    rows: [
-      { view: "toolbar", padding:3, elements: [
-        { view: "icon", icon: "mdi mdi-menu", click: function(){
-           $$("$sidebar1").toggle();
-         }
-        },
-        { view: "label", label: "Task manager"},
-        {},
-        { view: "icon", icon: "mdi mdi-comment",  badge:4},
-        { view:"button", value:"Выход", width:70}
-      ]
-      },
-      { cols:[
-        {
-          view: "sidebar",
-          data: menu_data,
-          on:{
-            onAfterSelect: function(id){
-              webix.message("Selected: "+this.getItem(id).value)
-              $$("form1").showBatch(id);
-            }
-          }
-        },
-        {type:"clean",rows:[
-        my_form,
-        {}
-        ]}
-      ]}
-    ]
-  });
-
+import { Application } from './js/src/components/Application.js';
+  webix.ready(() => {
+   let app = new Application();
+   app.init()
+   webix.ui(app.config())
+   app.attachEvents()
+})
