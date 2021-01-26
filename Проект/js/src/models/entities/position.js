4a4a93c59a6f0d3f13fd8208e1de6e41819d9585
1DarkSpectre1 <75720434+1DarkSpectre1@users.noreply.github.com>
2021-01-26 13:17:49 +0400

    Delete Проект directory

--- Проект/js/src/models/entities/position.js
+++
@@ -1,7 +0,0 @@
-// Position класс для представления сущности должности сотрудников
-export class Position {
-    constructor(id, name) {
-        this.ID = id
-        this.name = name
-    }
-}
--- Проект/js/src/models/entities/progect.js
+++
@@ -1,9 +0,0 @@
-// Employee класс для представления сущности сотрудника
-export class Progect {
-    constructor(id, name, employee) {
-        this.ID = id
-        this.name = name
-        this.employee = employee
-        
-    }
-}
