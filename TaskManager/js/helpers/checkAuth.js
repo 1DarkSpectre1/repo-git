7e957bb6f789a0748ada1267eacaa9adc6a904f7
1DarkSpectre1 <75720434+1DarkSpectre1@users.noreply.github.com>
2021-02-03 15:57:21 +0400

    Delete TaskManager/js directory

--- TaskManager/js/helpers/checkAuth.js
+++
@@ -1,9 +0,0 @@
-import authModel from "../src/models/authModel.js"
-
-// функция проверяет авторизованность клиента
-// вызывает callback функцию с boolean аргументом - признаком авторизованности
-export function checkAuth(callback) {
-    authModel.check().then((isAuthorize) => {
-        callback(isAuthorize)
-    })
-}
--- TaskManager/js/src/models/entities/user.js
+++
@@ -1,7 +0,0 @@
-// User класс для представления сущности пользователя
-export class User {
-    constructor(login, password) {
-        this.Login = login
-        this.Password = password
-    }
-}
